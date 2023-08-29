ea44bb9f3e72135ceef996c30fa25dedd8934ffb
ruksanasemeir <ruksana.semeir@tarento.com>
2023-08-29 17:30:53 +0530

    Exceptions have added. Create and update Api of comment is added for generalized json

--- src/main/java/com/tarento/commenthub/repository/CommentRepository.java
+++ src/main/java/com/tarento/commenthub/repository/CommentRepository.java
@@ -3,5 +3,8 @@ package com.tarento.commenthub.repository;
 import com.tarento.commenthub.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, String> {
+    Optional<Comment> findByIdAndStatus(String id, Boolean isActive);
 }
--- src/main/java/com/tarento/commenthub/service/impl/CommentServiceImpl.java
+++ src/main/java/com/tarento/commenthub/service/impl/CommentServiceImpl.java
@@ -1,5 +1,8 @@
 package com.tarento.commenthub.service.impl;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.uuid.Generators;
 import com.tarento.commenthub.entity.Comment;
 import com.tarento.commenthub.exception.CommentException;
@@ -20,29 +23,80 @@ public class CommentServiceImpl implements CommentService {
     private CommentRepository commentRepository;
 
     @Override
-    public Comment addComment(Comment comment) {
+    public Comment addComment(JsonNode comment) {
         log.info("CommentServiceImpl::addComment:adding comment");
         try {
             UUID uuid = Generators.timeBasedGenerator().generate();
             String id = uuid.toString();
-            comment.setId(id);
-            return commentRepository.save(comment);
+            ObjectNode objectComment = (ObjectNode) comment;
+            objectComment.put("id",id);
+            Comment commentDetailsFromJson = new Comment();
+            commentDetailsFromJson = fetchDetailsofComment(comment);
+            return commentRepository.save(commentDetailsFromJson);
         } catch (Exception e) {
             throw new CommentException("ERROR01", "Failed to coment");
         }
     }
 
     @Override
-    public Comment updateComment(Comment updatedComment) {
+    public Comment updateComment(JsonNode updatedComment) {
         log.info("CommentServiceImpl::updateComment:updating comment");
-        if (updatedComment.getId() != null){
-            Optional<Comment> fetchedComment = commentRepository.findById(updatedComment.getId());
+        Comment commentDetailsFromJson = new Comment();
+        commentDetailsFromJson = fetchDetailsofComment(updatedComment);
+        if (commentDetailsFromJson.getId() != null){
+            Optional<Comment> fetchedComment = commentRepository.findById(commentDetailsFromJson.getId());
             if (fetchedComment.isPresent() && fetchedComment.get().isStatus()){
-                return commentRepository.save(updatedComment);
+                return commentRepository.save(commentDetailsFromJson);
             }
         }else {
             throw new CommentException("ERROR02", "This comment is not present to edit ");
         }
-        return updatedComment;
+        return commentDetailsFromJson;
+    }
+
+    @Override
+    public Comment getCommentById(String id) {
+        log.info("CommentServiceImpl::getCommentById:fetching comment");
+        Optional<Comment> fetchedComment =  commentRepository.findById(id);
+        if(fetchedComment.isEmpty() ){
+            if(!fetchedComment.get().isStatus()) {
+                throw new CommentException("ERROR", "Comment is not found");
+            }else {
+                throw new CommentException("ERROR", "Its a deleted comment");
+            }
+        }
+        return fetchedComment.get();
+    }
+
+    @Override
+    public String deleteCommentById(String commentId) {
+        log.info("CommentServiceImpl::deleteCommentById:deleting comment");
+        Optional<Comment> fetchedComment =  commentRepository.findById(commentId);
+        if(fetchedComment.isPresent()){
+            if (fetchedComment.get().isStatus()){
+                Comment comment = new Comment();
+                comment = fetchedComment.get();
+                comment.setStatus(false);
+                commentRepository.save(comment);
+            }else{
+                throw new CommentException("ERROR03", "You are trying to delete a already deleted comment");
+            }
+        }else {
+            throw new CommentException("ERROR04", "No such comment found");
+        }
+        return null;
+    }
+
+    public Comment fetchDetailsofComment(JsonNode comment){
+        Comment commentFetched = new Comment();
+        commentFetched.setId(comment.get("id").asText());
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode commentJson = objectMapper.createObjectNode();
+        ObjectNode commentObjNode = (ObjectNode) commentJson;
+        commentObjNode.put("comment", comment.get("comment"));
+        commentObjNode.put("file", comment.get("file"));
+        commentObjNode.put("commentSource", comment.get("commentSource"));
+        commentFetched.setCommentJson(commentJson);
+        return commentFetched;
     }
 }
