c29de0b955f4ee836cc763848e817c3173711814
Rahul Baliram Pawar <rahul>
2023-08-30 11:59:14 +0530

    Added changes

---
+++ src/main/java/com/tarento/commenthub/constant/Constants.java
@@ -0,0 +1,16 @@
+package com.tarento.commenthub.constant;
+
+public class Constants {
+
+    public static final String ENTITY_ID = "entityId";
+
+    public static final String ENTITY_TYPE = "entitytype";
+
+    public static final String WORKFLOW = "workflow";
+
+    public static final String COMMENT_ID = "commentId";
+
+    public static final String COMMENT_TREE_ID = "commentTreeId";
+
+
+}
